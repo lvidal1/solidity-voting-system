1f3ad96e0327b58acbc3bf27a3cc87fdeae00192
lvidal1 <lvidal910@gmail.com>
2022-07-27 00:08:05 -0500

    [Contract] Implement addVote

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,7 +2,23 @@
 pragma solidity ^0.8.9;
 
 contract Voting {
+    uint256[2] public candidates;
+
+    constructor() {}
+
     function name() public pure returns (string memory) {
         return "Voting System 1.0";
+    }
+
+    function addVote(uint256 _votes) public payable {
+        candidates[msg.value] += _votes;
+    }
+
+    function getVotesCandidate1() external view returns (uint256) {
+        return candidates[0];
+    }
+
+    function getVotesCandidate2() external view returns (uint256) {
+        return candidates[1];
     }
 }
